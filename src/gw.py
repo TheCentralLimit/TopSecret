14d17ca4df55071eb5fa0e161845d7c946b07cbf
Daniel Wysocki <dwysocki@oswego.edu>
2016-04-28 19:55:18 -0400

    Basic gravitational wave calculations

---
+++ src/gw.py
@@ -0,0 +1,15 @@
+from __future__ import division
+import numpy as np
+
+def symmetric_mass_ratio(m_1, m_2):
+    M = m_1 + m_2
+
+    with np.errstate(invalid="ignore", divide="ignore"):
+        return m_1 * m_2 * M**-2
+
+
+def chirp_mass(m_1, m_2):
+    M = m_1 + m_2
+    eta = symmetric_mass_ratio(m_1, m_2)
+
+    return eta**(3/5) * M
