1d2500b2f31fcadf424ab2533f991184778eff29
Daniel Wysocki <dwysocki@oswego.edu>
2016-04-28 20:26:07 -0400

    Added the mass ratio, and some comments.

--- src/gw.py
+++ src/gw.py
@@ -1,11 +1,31 @@
+"""
+Basic gravitational wave calculations.
+"""
 from __future__ import division
 import numpy as np
 
 
 def mass_ratio(m_1, m_2):
-    pass
+    """
+    Returns the mass ratio q, defined such that the larger mass is in the
+    numerator, and the smaller mass is in the denominator.
+    """
+    # Initialize array.
+    q = np.empty_like(m_1)
+    # Indices where m_1 or m_2 are bigger.
+    m_1_bigger = m_1 > m_2
+    m_2_bigger = ~m_1_bigger
+    # Calculate the mass ratios for the two cases
+    q[m_1_bigger] = m_1[m_1_bigger] / m_2[m_1_bigger]
+    q[m_2_bigger] = m_2[m_2_bigger] / m_1[m_2_bigger]
+
+    return q
+
 
 def symmetric_mass_ratio(m_1, m_2):
+    """
+    Returns the symmetric mass ratio, eta.
+    """
     M = m_1 + m_2
 
     with np.errstate(invalid="ignore", divide="ignore"):
@@ -13,6 +33,9 @@ def symmetric_mass_ratio(m_1, m_2):
 
 
 def chirp_mass(m_1, m_2):
+    """
+    Returns the chirp mass, M_c.
+    """
     M = m_1 + m_2
     eta = symmetric_mass_ratio(m_1, m_2)
 
