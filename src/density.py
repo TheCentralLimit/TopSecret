dec1ec29c0b08993e06bedfa4764839c82ebf89f
Daniel Wysocki <dwysocki@oswego.edu>
2016-04-29 13:52:51 -0400

    Initial code for density estimation

--- src/density.py
+++ src/density.py
@@ -1,3 +1,13 @@
 from __future__ import division, print_function
 import numpy as np
+from statsmodels.nonparametric.kde import KDEUnivariate
 
+
+def density_estimator(M_c, V, T, **kwargs):
+    x = np.log10(M_c)
+    w = 1 / (V*T)
+
+    kde = KDEUnivariate(x)
+    kde.fit(weights=w, fft=False, **kwargs)
+
+    return kde # placeholder, return something different in the future
