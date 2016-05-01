973997b62ddff25adb40aa1e5d521d7a7f1f36cd
Daniel Wysocki <dwysocki@oswego.edu>
2016-05-01 17:07:00 -0400

    Preliminary density estimation plots

--- src/dans_code.py
+++ src/dans_code.py
@@ -1,9 +1,34 @@
 from __future__ import division, print_function
 from density import density_estimator
 
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+
+def chirp_mass_distribution(M_c, V, T):
+    kde = density_estimator(M_c, V, 1, kernel="gau", bw="scott")
+
+    x_smooth = np.linspace(0, np.log10(np.max(M_c)),
+                           num=1000)
+
+    r = kde.evaluate(x_smooth)
+
+    fig, (ax_pdf, ax_cdf) = plt.subplots(2)
+
+    ax_pdf.plot(x_smooth, r)
+
+    ax_pdf.semilogy()
+
+
+    ax_pdf.set_xlabel(r"$x$")
+    ax_pdf.set_ylabel(r"$\hat{r}$")
+
+    ax_cdf.plot(kde.cdf)
+
+    plt.show()
 
 
 def dans_code(m_1 , m_2, s, rho, q, eta, M_c, V):
-    kde = density_estimator(M_c, V, 1)
-
-    print(kde.cdf)
+    chirp_mass_distribution(M_c, V, 1)
